07f7ec1bcd1977918ad952870c668e2c840b8383
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-04 15:09:36 +0200

    feat: get numbers from localstorage when loading page

--- Lab 2/js/bingo.js
+++ Lab 2/js/bingo.js
@@ -106,7 +106,8 @@ export default class Bingo {
 
     // check if localstorage item exists
     if (localStorage.getItem("bingo")) {
-      // let cardsWon = JSON.parse();
+      let cardsWon = JSON.parse(localStorage.getItem("bingo"));
+    console.log(cardsWon);
       // JSON.parse() will convert the string [1, 7, 8] back to an array which you can loop
       // loop over the numbers 1, 7, 8 and mark those cards as done by adding the right CSS class
       // .bingo__card--done
