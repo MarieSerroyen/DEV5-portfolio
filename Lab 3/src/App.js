8bc82d5839a3d86c87dda9d00290a778a41d1554
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-05 12:25:01 +0200

    feat: geLocation function works

--- Lab 3/src/App.js
+++ Lab 3/src/App.js
@@ -2,5 +2,11 @@ export default class App {
     constructor(api_key) {
         this.apiKey = api_key;
         //console.log("Class works!! " + this.apiKey);
+        this.getLocation();
     }
+
+    getLocation() {
+        console.log("Getting the location");
+    }
+
 }
