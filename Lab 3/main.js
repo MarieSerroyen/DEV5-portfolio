1e787f0f729b0b0206f0d6eeddc395f1c7e260bb
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-05 12:23:24 +0200

    feat: app class works and getting api key from main.js

--- Lab 3/main.js
+++ Lab 3/main.js
@@ -1,2 +1,5 @@
 import './style.css'
 
+import App from "./src/App.js";
+
+const app = new App("7582e9527e94c39375d30a701e0db911");
---
+++ Lab 3/src/App.js
@@ -0,0 +1,6 @@
+export default class App {
+    constructor(api_key) {
+        this.apiKey = api_key;
+        //console.log("Class works!! " + this.apiKey);
+    }
+}
