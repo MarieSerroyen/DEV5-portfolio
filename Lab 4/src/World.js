42b0182d68f7cfd517b39e8904251ca1fa74496b
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-15 09:09:49 +0200

    feat: add sky

--- Lab 4/src/World.js
+++ Lab 4/src/World.js
@@ -7,6 +7,11 @@ export default class World {
     }
 
     createSky() {
-        console.log("It works");
+        //console.log("It works");
+        const skyGeometry = new THREE.SphereGeometry( 100, 32, 32 );
+        const skyMaterial = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
+        skyMaterial.side = THREE.BackSide;
+        const sky = new THREE.Mesh( skyGeometry, skyMaterial );
+        this.group.add( sky );
     }
 }
