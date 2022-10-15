35042f9a30ac45d810f8b9306e7070fbbe3d5075
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-15 11:30:40 +0200

    feat: add outer wall

--- Lab 4/src/Wall.js
+++ Lab 4/src/Wall.js
@@ -6,6 +6,7 @@ export default class Wall {
 		this.group = new THREE.Group();
         this.createTowers();
         this.createTowerWindows();
+        this.createWalls();
     }
 
     createTowers() {
@@ -40,5 +41,28 @@ export default class Wall {
         window2.position.set(20, 8, 28.6);
 		
         this.group.add( window1, window2);
+    }
+
+    createWalls() {
+        const textureLoader = new THREE.TextureLoader();
+        const grainsTexture = textureLoader.load('assets/textures/grains.jpg');
+
+        const geometryBack = new THREE.BoxGeometry( 36, 8, 1 );
+        const geometrySide = new THREE.BoxGeometry( 44, 8, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0xF7D291 } );
+        material.map = grainsTexture;	
+
+        const backOuterWall = new THREE.Mesh( geometryBack, material );
+        const leftOuterWall = new THREE.Mesh( geometrySide, material );
+        const rightOuterWall = new THREE.Mesh( geometrySide, material );
+
+        backOuterWall.position.set(0, 3.5, -25);
+        leftOuterWall.position.set(-21, 3.5, 0);
+        leftOuterWall.rotation.y = Math.PI / 2;
+        rightOuterWall.position.set(21, 3.5, 0);
+        rightOuterWall.rotation.y = Math.PI / 2;
+
+        this.group.add( backOuterWall, leftOuterWall, rightOuterWall);
+
     }
 }
