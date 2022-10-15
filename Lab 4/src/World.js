2845d9bdef556315234be4641ae15d4346696f46
MarieSerroyen <marie.serroyen@gmail.com>
2022-10-15 09:23:39 +0200

    feat: add sand texture to ground

--- Lab 4/src/World.js
+++ Lab 4/src/World.js
@@ -17,8 +17,12 @@ export default class World {
     }
 
     createSand() {
+        const textureLoader = new THREE.TextureLoader();
+        const sandTexture = textureLoader.load('assets/textures/sand.jpg');
+
         const sandGeometry = new THREE.BoxGeometry( 70, 0.1, 70 );
-        const sandMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+        const sandMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff} );
+        sandMaterial.map = sandTexture;
         const sand = new THREE.Mesh( sandGeometry, sandMaterial );
         sand.position.y = -0.05;
         this.group.add( sand );
